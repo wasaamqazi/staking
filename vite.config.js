147ba6a9729e38d8629a0157d9ebba20d772d800
Qasim Amin <qasim2646@gmail.com>
2023-03-08 11:02:33 +0500

    Commit iwht buffer true added

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,19 @@ export default defineConfig({
   define: {
     global: "globalThis",
   },
+  // ...other config settings
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
+  }
 });
