aea0d5f331f904c0b86dcc45d50925c7b1da8e2a
Qasim Amin <qasim2646@gmail.com>
2023-03-08 11:20:36 +0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import inject from "@rollup/plugin-inject";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,27 +8,6 @@ export default defineConfig({
   server: {
     host: true,
   },
-  define: {
-    global: "globalThis",
-  },
-  // ...other config settings
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis'
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-      ]
-    }
-  },
-  build: {
-    rollupOptions: {
-      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
-    },
-  },
+
+
 });
